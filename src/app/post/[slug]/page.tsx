3bac595347059d8ca692a21a5d8c0ef4e1fdfea0
Jessica Santos <jel_008@hotmail.com>
2025-10-18 15:06:51 -0300

    Create componet Single Post and

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,18 +1,32 @@
+import { SinglePost } from "@/components/SinglePost";
+import { SpinLoader } from "@/components/SpinLoader";
 import { findPostBySlugCached } from "@/lib/post/queries";
-import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import { Suspense } from "react";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 
+export async function genereteMetaData({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostBySlugCached(slug);
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
+
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
-  
   const post = await findPostBySlugCached(slug);
- 
+
   return (
-    <div>
-      <p>{post.content}</p>
-    </div>
+    <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
+      <SinglePost slug={slug} />
+    </Suspense>
   );
 }
